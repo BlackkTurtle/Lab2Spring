1286d764ca4f9c909a3a8a4f457a5128bb0f3bb4
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-16 13:54:59 +0300

    Initial commit

---
+++ src/main/java/org/o7planning/thymeleaf/Lab2SpringApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2SpringApplication.class, args);
+	}
+
+}
